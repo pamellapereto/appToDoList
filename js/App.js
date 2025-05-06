69bcf37272d8334a85c591b355c9a42682eaa2af
Pamella Pereto <pamellapereto@gmail.com>
2025-05-06 19:28:06 -0300

    Atualização

--- js/App.js
+++ js/App.js
@@ -1,12 +1,11 @@
 //Array para armazenar as tarefas
-var tarefas = [];
+let tarefas = [];
+
+const inputTarefa = document.querySelector(".campo-tarefa"); //<input>
+const botaoAdicionar = document.querySelector(".botao-adicionar"); //<button>
+const listaTarefas = document.querySelector(".lista-tarefas"); //<ul>
  
-const inputTarefa = document.querySelector(".campo-tarefa");
-const botaoAdicionar = document.querySelector(".botao-adicionar");
-const botaoEditar = document.querySelector(".botao-editar");
-const botaoRemover = document.querySelector(".botao-remover");
-const listaTarefas = document.querySelector(".lista-tarefas");
- 
+
 //Função para adicionar uma tarefa
 function renderizarTarefas(){
     listaTarefas.innerHTML = "";
@@ -14,26 +13,31 @@ function renderizarTarefas(){
     //para percorrer a array com as tarefas alocadas e posicionadas na tela (i)
     for (var i = 0; i < tarefas.length; i++){
         const tarefaTexto = tarefas[i];
-   
- 
+
         //cria um elemento li para cada tarefa
- 
-    const itemLista = document.createElement("li");
-    li.nomeClasse = "item-tarefa";
-    li.textContent = tarefaTexto;
- 
-    botaoRemover = document.createElement("button");
-    botaoRemover.className = "botao-remover";
-    botaoRemover.textContent = "Remover";
- 
-    //adiciona o botão (clique) de remover a tarefa
-    botaoRemover.addEventListener("click", function() {
+        const itemLista = document.createElement("li");
+        itemLista.className = "item-tarefa";
+        itemLista.textContent = tarefaTexto;
+
+        //Cria o elemento botão remover para cada tarefa
+        const botaoRemover = document.createElement("button");
+        botaoRemover.className = "botao-remover";
+        botaoRemover.textContent = "Remover";
+
+        //Cria o elemento botão editar para cada tarefa
+        const botaoEditar = document.createElement("button");
+        botaoEditar.className = "botao-editar";
+        botaoEditar.textContent = "Editar";
+
+       //adiciona o botão (clique) de remover a tarefa
+       botaoRemover.addEventListener("click", function(i) {
         tarefas.splice(i, 1); //remove a tarefa do array
         renderizarTarefas(); //atualiza a lista de tarefas
-    });
- 
-    li.appendChild(botaoRemover); //adiciona o botão de remover a tarefa
-    listaTarefas.appendChild(li); //adiciona a tarefa na lista de tarefas
+        }
+    );
+
+     li.appendChild(botaoRemover); //adiciona o botão de remover a tarefa
+     listaTarefas.appendChild(li); //adiciona a tarefa na lista de tarefas
     }
  
     // Evento para adicionar a tarefa
@@ -48,4 +52,3 @@ function renderizarTarefas(){
        
     });
 }
- 
